1d141b65de5de0063a81c8c0aa44b4a1d3e6cb3a
Fergie McDowall <fergusmcdowall@gmail.com>
2016-03-15 15:03:34 +0100

    better separator handling from updated term-vector, term-frequency. Linting

--- indexer.js
+++ indexer.js
@@ -1,15 +1,16 @@
-/*jshint -W083 */ //makes jslint overlook functions in lodash for-loops
-const _ = require('lodash');
-var async = require('async');
-var hash = require('object-hash');
-var tv = require('term-vector');
-var tf = require('term-frequency');
-var skeleton = require('log-skeleton');
+const _ = require('lodash')
+var async = require('async')
+var hash = require('object-hash')
+var tv = require('term-vector')
+var tf = require('term-frequency')
+var skeleton = require('log-skeleton')
+var sid = require('search-index-deleter')
 
 module.exports = function (givenOptions, callback) {
-  var Indexer = {};
-  getOptions(givenOptions, function(err, options) {
-    
+  var Indexer = {}
+  getOptions(givenOptions, function (err, options) {
+    if (err) callback(err, null)
+
     Indexer.close = function (callback) {
       options.indexes.close(function (err) {
         while (!options.indexes.isClosed()) {
@@ -22,27 +23,30 @@ module.exports = function (givenOptions, callback) {
       })
     }
 
-
     Indexer.options = options
-    require('search-index-deleter')(Indexer.options, function(err, deleter) {
-      var log = skeleton((Indexer.options) ? Indexer.options.log : undefined);
-      var q = async.queue(function (batch, callback) {
-        batch.options = _.defaults(batch.options, options);
-        //generate IDs if none are present and stringify numeric IDs
-        var salt = 0;
-        batch.data.map(function (doc) {
-          if (!doc.id)
-            doc.id = (++salt) + '-' + hash(doc);
-          doc.id = doc.id + ''; // stringify ID
-        });
-        deleter.deleteBatch(_.map(batch.data, 'id'), function (err) {
-          if (err) log.info(err);
-          addBatch(batch.data, batch.options, function(err) {
-            return callback(err);
-          });
-        });
-      }, 1);
 
+    sid(Indexer.options, function (err, deleter) {
+      if (err) {
+        callback(err, null)
+      }
+      var log = skeleton((Indexer.options) ? Indexer.options.log : undefined)
+      var q = async.queue(function (batch, callback) {
+        batch.options = _.defaults(batch.options, options)
+        // generate IDs if none are present and stringify numeric IDs
+        var salt = 0
+        batch.data.map(function (doc) {
+          if (!doc.id) {
+            doc.id = (++salt) + '-' + hash(doc)
+          }
+          doc.id = doc.id + '' // stringify ID
+        })
+        deleter.deleteBatch(_.map(batch.data, 'id'), function (err) {
+          if (err) log.info(err)
+          addBatch(batch.data, batch.options, function (err) {
+            return callback(err)
+          })
+        })
+      }, 1)
 
       Indexer.add = function (batch, batchOptions, callback) {
         if (arguments.length === 2 && _.isFunction(arguments[1])) {
@@ -50,75 +54,75 @@ module.exports = function (givenOptions, callback) {
           batchOptions = undefined
         }
         addBatchToIndex(batch,
-                        batchOptions,
-                        callback)
+          batchOptions,
+          callback)
       }
 
-
-      Indexer.getOptions = function() {
+      Indexer.getOptions = function () {
         return Indexer.options
       }
-      
-      var addBatchToIndex = function (batch, batchOptions, callback) {
-        batchOptions = processBatchOptions(Indexer.options, batchOptions);
-        if (!_.isArray(batch) && _.isPlainObject(batch)) {
-          batch = [batch];
-        }
-        q.push({data:batch, options:batchOptions}, callback)
-      };
 
+      var addBatchToIndex = function (batch, batchOptions, callback) {
+        batchOptions = processBatchOptions(Indexer.options, batchOptions)
+        if (!_.isArray(batch) && _.isPlainObject(batch)) {
+          batch = [batch]
+        }
+        q.push({data: batch, options: batchOptions}, callback)
+      }
 
       var removeInvalidFields = function (doc) {
         for (var fieldKey in doc) {
-          if (_.isArray(doc[fieldKey])) continue;
+          if (_.isArray(doc[fieldKey])) continue
           else if (doc[fieldKey] === null) {
-            delete doc[fieldKey];
-            log.warn(doc.id + ': ' + fieldKey + ' field is null, SKIPPING');
-          }
-          //only index fields that are strings or numbers
-          else if (!(_.isString(doc[fieldKey]) || _.isNumber(doc[fieldKey]))) {
-            delete doc[fieldKey];
+            delete doc[fieldKey]
+            log.warn(doc.id + ': ' + fieldKey + ' field is null, SKIPPING')
+            // only index fields that are strings or numbers
+          } else if (!(_.isString(doc[fieldKey]) || _.isNumber(doc[fieldKey]))) {
+            delete doc[fieldKey]
             log.warn(doc.id + ': ' + fieldKey +
-                     ' field not string or array, SKIPPING');
+                     ' field not string or array, SKIPPING')
           }
         }
-        return doc;
-      };
+        return doc
+      }
 
-
-      function getIndexEntries(doc, batchOptions) {
-        var docIndexEntries = [];
-        if (!_.isPlainObject(doc))
-          return callbacky(new Error('Malformed document'), {});
-        doc = removeInvalidFields(doc);
-        if (batchOptions.fieldsToStore == 'all')
-          batchOptions.fieldsToStore = Object.keys(doc);
-        log.info('indexing ' + doc.id);
+      function getIndexEntries (doc, batchOptions) {
+        var docIndexEntries = []
+        if (!_.isPlainObject(doc)) {
+          return callback(new Error('Malformed document'), {})
+        }
+        doc = removeInvalidFields(doc)
+        if (batchOptions.fieldsToStore === 'all') {
+          batchOptions.fieldsToStore = Object.keys(doc)
+        }
+        log.info('indexing ' + doc.id)
         docIndexEntries.push({
           type: 'put',
           key: 'DOCUMENT￮' + doc.id + '￮',
-          value:  _.pick(doc, batchOptions.fieldsToStore)
-        });
-        var freqsForComposite = []; //put document frequencies in here
+          value: _.pick(doc, batchOptions.fieldsToStore)
+        })
+        var freqsForComposite = [] // put document frequencies in here
         _.forEach(doc, function (field, fieldName) {
-          var fieldOptions = _.defaults(_.find(batchOptions.fieldOptions, ['fieldName', fieldName]) || {}, batchOptions.defaultFieldOptions);
-
-          if (fieldName == 'id') fieldOptions.stopwords = '';   // because you cant run stopwords on id field
-          else fieldOptions.stopwords = batchOptions.stopwords;
-          if (_.isArray(field)) field = field.join(' '); // make filter fields searchable
+          var fieldOptions = _.defaults(_.find(batchOptions.fieldOptions, ['fieldName', fieldName]) || {}, batchOptions.defaultFieldOptions)
+          if (fieldName === 'id') {
+            fieldOptions.stopwords = '' // because you cant run stopwords on id field
+          } else {
+            fieldOptions.stopwords = batchOptions.stopwords
+          }
+          if (_.isArray(field)) field = field.join(' ') // make filter fields searchable
           var v = tv.getVector(field + '', {
-            separator: batchOptions.separator,
+            separator: fieldOptions.separator,
             stopwords: fieldOptions.stopwords,
             nGramLength: fieldOptions.nGramLength
-          });
-          v.push(['*', 1]); //can do wildcard searh on this field
+          })
+          v.push([['*'], 1]) // can do wildcard searh on this field
           var freq = tf.getTermFrequency(v, {
             scheme: 'doubleLogNormalization0.5',
             weight: fieldOptions.weight
-          });
-          if (fieldOptions.searchable)
-            freqsForComposite.push(freq);
-          var deleteKeys = [];
+          })
+          if (fieldOptions.searchable) {
+            freqsForComposite.push(freq)
+          }
           if (fieldOptions.fieldedSearch) {
             freq.forEach(function (item) {
               batchOptions.filters.forEach(function (filter) {
@@ -127,39 +131,40 @@ module.exports = function (givenOptions, callback) {
                     type: 'put',
                     key: 'TF￮' + fieldName + '￮' + item[0] + '￮' + filter + '￮' + filterKey,
                     value: [doc.id]
-                  });
+                  })
                   docIndexEntries.push({
                     type: 'put',
                     key: 'RI￮' + fieldName + '￮' + item[0] + '￮' + filter + '￮' + filterKey,
                     value: [[item[1].toFixed(16), doc.id]]
-                  });
-                });
-              });
+                  })
+                })
+              })
               docIndexEntries.push({
                 type: 'put',
                 key: 'TF￮' + fieldName + '￮' + item[0] + '￮￮',
                 value: [doc.id]
-              });
+              })
               docIndexEntries.push({
                 type: 'put',
                 key: 'RI￮' + fieldName + '￮' + item[0] + '￮￮',
                 value: [[item[1].toFixed(16), doc.id]]
-              });
-            });
-          };
-        });
-        //generate * field
+              })
+            })
+          }
+        })
+        // generate * field
         _(freqsForComposite)
           .flatten()
           .sort()
           .reduce(function (prev, item) {
-            if (!prev[0]) prev.push(item);
-            else if (item[0] == _.last(prev)[0]) {
-              _.last(prev)[1] = _.last(prev)[1] + item[1];
+            if (!prev[0]) prev.push(item)
+            //            else if (item[0] == _.last(prev)[0]) {
+            else if (_.isEqual(item[0], _.last(prev)[0])) {
+              _.last(prev)[1] = _.last(prev)[1] + item[1]
+            } else {
+              prev.push(item)
             }
-            else
-              prev.push(item);
-            return prev;
+            return prev
           }, [])
           .forEach(function (item) {
             batchOptions.filters.forEach(function (filter) {
@@ -168,120 +173,122 @@ module.exports = function (givenOptions, callback) {
                   type: 'put',
                   key: 'TF￮*￮' + item[0] + '￮' + filter + '￮' + filterKey,
                   value: [doc.id]
-                });
+                })
                 docIndexEntries.push({
                   type: 'put',
                   key: 'RI￮*￮' + item[0] + '￮' + filter + '￮' + filterKey,
                   value: [[item[1].toFixed(16), doc.id]]
-                });
-              });
-            });
+                })
+              })
+            })
             docIndexEntries.push({
               type: 'put',
               key: 'TF￮*￮' + item[0] + '￮￮',
               value: [doc.id]
-            });
+            })
             docIndexEntries.push({
               type: 'put',
               key: 'RI￮*￮' + item[0] + '￮￮',
               value: [[item[1].toFixed(16), doc.id]]
-            });
-          });
+            })
+          })
         docIndexEntries.push({
           type: 'put',
           key: 'DELETE-DOCUMENT￮' + doc.id,
           value: _.map(docIndexEntries, 'key')
-        });
-        return docIndexEntries;
+        })
+        return docIndexEntries
       }
 
-      function addBatch(batch, batchOptions, callbackster) {
-        var dbInstructions = [];
+      function addBatch (batch, batchOptions, callbackster) {
+        var dbInstructions = []
         batch.forEach(function (doc) {
-          dbInstructions.push(getIndexEntries(doc, batchOptions));
-        });
+          dbInstructions.push(getIndexEntries(doc, batchOptions))
+        })
         dbInstructions.push({
           type: 'put',
           key: 'DOCUMENT-COUNT',
           value: batch.length
-        });
+        })
         dbInstructions = _(dbInstructions)
           .flatten()
           .sortBy('key')
           .reduce(function (prev, item) {
-            if (item.key.substring(0, 6) == 'DELETE')
-              prev.push(item);
-            else if (item.key.substring(0, 8) == 'DOCUMENT')
-              prev.push(item);
-            else if (item.key.substring(0, 2) == 'RI') {
-              if (item.key == _.last(prev).key)
-                _.last(prev).value.push(item.value[0]);
-              else
-                prev.push(item);
+            if (item.key.substring(0, 6) === 'DELETE') {
+              prev.push(item)
+            } else if (item.key.substring(0, 8) === 'DOCUMENT') {
+              prev.push(item)
+            } else if (item.key.substring(0, 2) === 'RI') {
+              if (item.key === _.last(prev).key) {
+                _.last(prev).value.push(item.value[0])
+              } else {
+                prev.push(item)
+              }
+            } else if (item.key.substring(0, 2) === 'TF') {
+              if (item.key === _.last(prev).key) {
+                _.last(prev).value = _.last(prev).value.concat(item.value)
+              } else {
+                prev.push(item)
+              }
             }
-            else if (item.key.substring(0, 2) == 'TF') {
-              if (item.key == _.last(prev).key)
-                _.last(prev).value = _.last(prev).value.concat(item.value);
-              else
-                prev.push(item);
-            }
-            return prev;
-          }, []);
+            return prev
+          }, [])
         async.eachSeries(
           dbInstructions,
           function (item, callback) {
             Indexer.options.indexes.get(item.key, function (err, val) {
-              if (item.key.substring(0, 2) == 'TF') {
-                if (val)
-                  item.value = item.value.concat(val);
-                item.value = item.value.sort();
-              }
-              else if (item.key.substring(0, 2) == 'RI') {
-                if (val)
-                  item.value = item.value.concat(val);                
+              if (err) log.warn(err)
+              if (item.key.substring(0, 2) === 'TF') {
+                if (val) {
+                  item.value = item.value.concat(val)
+                }
+                item.value = item.value.sort()
+              } else if (item.key.substring(0, 2) === 'RI') {
+                if (val) {
+                  item.value = item.value.concat(val)
+                }
                 item.value = item.value.sort(function (a, b) {
-                  //sort buy score and then ID, descending:
+                  // sort buy score and then ID, descending:
                   if (b[0] > a[0]) return 1
                   if (b[0] < a[0]) return -1
                   if (b[1] > a[1]) return 1
                   if (b[1] < a[1]) return -1
                   return 0
-                });
-              }
-              else if (item.key == 'DOCUMENT-COUNT') {
+                })
+              } else if (item.key === 'DOCUMENT-COUNT') {
                 // console.log(val)
-                if (val)
-                  item.value = +val + +(item.value);
-                // console.log(item.value)
+                if (val) {
+                  item.value = +val + +(item.value)
+                }
               }
-              return callback(null);
-            });
+              return callback(null)
+            })
           },
           function (err) {
+            if (err) log.warn(err)
             dbInstructions.push({key: 'LAST-UPDATE-TIMESTAMP', value: Date.now()})
             Indexer.options.indexes.batch(dbInstructions, function (err) {
-              if (err) log.warn('Ooops!', err);
-              else log.info('batch indexed!');
-              return callbackster(null);
-            });
-          });
+              if (err) log.warn('Ooops!', err)
+              else log.info('batch indexed!')
+              return callbackster(null)
+            })
+          })
       }
 
-      //  return Indexer;
+      //  return Indexer
       return callback(null, Indexer)
     })
   })
 }
 
-
-var getOptions = function(givenOptions, callbacky) {
+var getOptions = function (givenOptions, callbacky) {
   const _ = require('lodash')
   const bunyan = require('bunyan')
   var levelup = require('levelup')
   const tv = require('term-vector')
   givenOptions = givenOptions || {}
   async.parallel([
-    function(callback) {
+    function (callback) {
       var defaultOps = {}
       defaultOps.deletable = true
       defaultOps.fieldedSearch = true
@@ -297,29 +304,26 @@ var getOptions = function(givenOptions, callbacky) {
       })
       callback(null, defaultOps)
     },
-    function(callback){
+    function (callback) {
       if (!givenOptions.indexes) {
         levelup(givenOptions.indexPath || 'si', {
           valueEncoding: 'json'
-        }, function(err, db) {
-          callback(null, db)
+        }, function (err, db) {
+          callback(err, db)
         })
-      }
-      else {
+      } else {
         callback(null, null)
       }
     }
-  ], function(err, results){
+  ], function (err, results) {
     var options = _.defaults(givenOptions, results[0])
     if (results[1] != null) {
-//      options = _.defaults(options, results[1])
+      //      options = _.defaults(options, results[1])
       options.indexes = results[1]
     }
     return callbacky(err, options)
   })
 }
-
-
 
 var processBatchOptions = function (siOptions, batchOptions) {
   var defaultFieldOptions = {
--- test/test.js
+++ test/test.js
@@ -1,27 +1,82 @@
-var _ = require('lodash');
+var _ = require('lodash')
 var da = require('distribute-array')
 var sia = require('../')
 var sis = require('search-index-searcher')
-var test = require('tape');
-var async = require('async');
+var test = require('tape')
+var async = require('async')
 
 var resultForStarUSA = [ '287', '510', '998', '997', '996', '995', '994', '993', '992', '991' ]
 
-test('simple indexing test', function (t) {
-  var batch = require('../node_modules/reuters-21578-json/data/full/reuters-000.json')
-  t.plan(6);
-  t.equal(batch.length, 1000);
-  sia({indexPath: 'test/sandbox/simpleIndexing'}, function(err, indexer) {
+test('set seperator at field level', function (t) {
+  t.plan(5)
+  var batch = [{
+    id: '1',
+    title: 'thisxisxaxtitle',
+    body: 'horsexzebraxelephant'
+  }, {
+    id: '2',
+    title: 'this is a title',
+    body: 'horse zebra elephant'
+  }]
+  sia({
+    indexPath: 'test/sandbox/separatorTest'
+  }, function (err, indexer) {
     t.error(err)
-    sis(indexer.getOptions(), function(err, searcher) {
+    indexer.add(batch, {
+      fieldOptions: [{
+        fieldName: 'body',
+        separator: 'x'}]
+    }, function (err) {
       t.error(err)
-      indexer.add(batch, {}, function(err) {
+      sis(indexer.getOptions(), function (err, searcher) {
+        t.error(err)
+        var q = {}
+        q.query = {'*': ['zebra']}
+        searcher.search(q, function (err, searchResults) {
+          t.error(err)
+          t.equal(searchResults.hits[0].id, '1')
+        })
+      })
+    })
+  })
+})
+
+test('simple indexing test - debugging 510', function (t) {
+  t.plan(6)
+  var batch = _.filter(require('../node_modules/reuters-21578-json/data/full/reuters-000.json'), { 'id': '510' })
+  t.equal(batch.length, 1)
+  sia({indexPath: 'test/sandbox/simpleIndexing510'}, function (err, indexer) {
+    t.error(err)
+    sis(indexer.getOptions(), function (err, searcher) {
+      t.error(err)
+      indexer.add(batch, {}, function (err) {
         t.error(err)
         var q = {}
         q.query = {'*': ['usa']}
         searcher.search(q, function (err, searchResults) {
           t.error(err)
-          t.deepLooseEqual(_.map(searchResults.hits, 'id').slice(0,10), resultForStarUSA)
+          t.equal(searchResults.hits[0].tf.length, 1)
+        })
+      })
+    })
+  })
+})
+
+test('simple indexing test', function (t) {
+  var batch = require('../node_modules/reuters-21578-json/data/full/reuters-000.json')
+  t.plan(6)
+  t.equal(batch.length, 1000)
+  sia({indexPath: 'test/sandbox/simpleIndexing'}, function (err, indexer) {
+    t.error(err)
+    sis(indexer.getOptions(), function (err, searcher) {
+      t.error(err)
+      indexer.add(batch, {}, function (err) {
+        t.error(err)
+        var q = {}
+        q.query = {'*': ['usa']}
+        searcher.search(q, function (err, searchResults) {
+          t.error(err)
+          t.deepLooseEqual(_.map(searchResults.hits, 'id').slice(0, 10), resultForStarUSA)
         })
       })
     })
@@ -30,33 +85,34 @@ test('simple indexing test', function (t) {
 
 test('concurrancy test', function (t) {
   var startTime = Date.now()
-  t.plan(18)
-  sia({indexPath: 'test/sandbox/concurrentIndexing'}, function(err, indexer) {
+  t.plan(19)
+  sia({indexPath: 'test/sandbox/concurrentIndexing'}, function (err, indexer) {
     t.error(err)
-    sis(indexer.getOptions(), function(err, searcher) {
+    sis(indexer.getOptions(), function (err, searcher) {
       t.error(err)
       var batchData = da(require('../node_modules/reuters-21578-json/data/full/reuters-000.json'), 10)
-      t.equal(batchData.length, 10);
-      async.each(batchData, function(batch, callback) {
+      t.equal(batchData.length, 10)
+      async.each(batchData, function (batch, callback) {
         console.log('task submitted')
-        indexer.add(batch, {}, function(err) {
+        indexer.add(batch, {}, function (err) {
           if (!err) t.pass('no errorness')
-          callback();
-        })    
-      }, function(err) {
+          callback()
+        })
+      }, function (err) {
+        t.error(err)
         var q = {}
         q.query = {'*': ['usa']} // TODO: add error message if this is
         //      not an array
         searcher.search(q, function (err, searchResults) {
           if (!err) t.pass('no errorness')
-          t.deepLooseEqual(_.map(searchResults.hits, 'id').slice(0,10), resultForStarUSA)
+          t.deepLooseEqual(_.map(searchResults.hits, 'id').slice(0, 10), resultForStarUSA)
         })
-        indexer.getOptions().indexes.get('LAST-UPDATE-TIMESTAMP', function(err, val) {
+        indexer.getOptions().indexes.get('LAST-UPDATE-TIMESTAMP', function (err, val) {
           t.error(err)
           t.ok((val - startTime) > 0,
-               'lastUpdateTimestamp seems reasonable (' + (val - startTime) + ')')
+            'lastUpdateTimestamp seems reasonable (' + (val - startTime) + ')')
           t.ok((val - startTime) < 60000,
-               'lastUpdateTimestamp seems reasonable (' + (val - startTime) + ')')
+            'lastUpdateTimestamp seems reasonable (' + (val - startTime) + ')')
         })
       })
     })
