094f71b579521e51909ad3a870ead0daaedd60ed
Fergie McDowall <fergusmcdowall@gmail.com>
2016-09-23 13:53:58 +0200

    better support for specifying searchability

--- test/merge-test.js
+++ test/merge-test.js
@@ -7,21 +7,25 @@ const zlib = require('zlib')
 
 const stones = [
   {
+    id: '1',
     name: 'Mick',
     band: ['stones'],
     bio: 'Jagger\'s career has spanned over 50 years, and he has been described as "one of the most popular and influential frontmen in the history of Rock & Roll".[7] Jagger\'s distinctive voice and performance, along with Keith Richards\' guitar style, have been the trademark of the Rolling Stones throughout the career of the band'
   },
   {
+    id: '3',
     name: 'Kieth',
     band: ['stones'],
     bio: 'Keith Richards (born 18 December 1943) is an English guitarist, singer, songwriter, best-selling memoirist, and founding member of the rock band The Rolling Stones. Rolling Stone Magazine credited Richards for "rock\'s greatest single body of riffs" on guitar and ranked him 4th on its list of 100 best guitarists.'
   },
   {
+    id: '5',
     name: 'Charlie',
     band: ['stones'],
     bio: 'Best known as a member of The Rolling Stones. Originally trained as a graphic artist, he started playing drums in London’s rhythm and blues clubs, where he met Brian Jones, Mick Jagger, and Keith Richards. In 1963, he joined their group, the Rolling Stones, as drummer, while doubling as designer of their record sleeves and tour stages'
   },
   {
+    id: '7',
     name: 'Ronnie',
     band: ['stones'],
     bio: 'English rock musician, singer, songwriter, artist and radio personality best known as a member of The Rolling Stones since 1975, as well as a member of Faces and the Jeff Beck Group.'
@@ -30,21 +34,25 @@ const stones = [
 
 const beatles = [
   {
+    id: '2',
     name: 'John',
     band: ['beatles'],
     bio: 'English singer and songwriter who co-founded the Beatles (1960-70), the most commercially successful band in the history of popular music. With fellow member Paul McCartney, he formed a lucrative songwriting partnership'
   },
   {
+    id: '4',
     name: 'Paul',
     band: ['beatles'],
     bio: 'English singer-songwriter, multi-instrumentalist, and composer. With John Lennon, George Harrison, and Ringo Starr, he gained worldwide fame with the rock band the Beatles, one of the most popular and influential groups in the history of pop music.'
   },
   {
+    id: '6',
     name: 'George',
     band: ['beatles'],
     bio: 'Often referred to as "the quiet Beatle",[3][4] Harrison embraced Indian mysticism and helped broaden the horizons of his fellow Beatles as well as their Western audience by incorporating Indian instrumentation in their music'
   },
   {
+    id: '8',
     name: 'Ringo',
     band: ['beatles'],
     bio: 'English musician, singer, songwriter and actor who gained worldwide fame as the drummer for the Beatles. He occasionally sang lead vocals, usually for one song on an album, including "With a Little Help from My Friends", "Yellow Submarine" and their cover of "Act Naturally"'
@@ -107,7 +115,7 @@ test('make the stones search index', function (t) {
 
 test('confirm can search as normal in beatles', function (t) {
   t.plan(6)
-  var results = [ 'John', 'Paul', 'George', 'Ringo' ]
+  var results = [ 'Ringo', 'George', 'Paul', 'John' ]
   SearchIndexSearcher({
     indexPath: 'test/sandbox/beatles'
   }, function (err, si) {
@@ -127,7 +135,7 @@ test('confirm can search as normal in beatles', function (t) {
 
 test('confirm can search as normal in stones', function (t) {
   t.plan(6)
-  var results = [ 'Mick', 'Kieth', 'Charlie', 'Ronnie' ]
+  var results = [ 'Ronnie', 'Charlie', 'Kieth', 'Mick' ]
   SearchIndexSearcher({
     indexPath: 'test/sandbox/stones'
   }, function (err, si) {
@@ -174,7 +182,7 @@ test('gzipped replication from beatles to supergroup', function (t) {
 
 test('supergroup contains beatles', function (t) {
   t.plan(6)
-  var results = [ 'John', 'Paul', 'George', 'Ringo' ]
+  var results = [ 'Ringo', 'George', 'Paul', 'John' ]
   SearchIndexSearcher({
     indexPath: 'test/sandbox/supergroup'
   }, function (err, si) {
@@ -221,7 +229,9 @@ test('gzipped replication of stones to supergroup', function (t) {
 
 test('open supergroup index', function (t) {
   t.plan(9)
-  var results = [ 'Mick', 'Kieth', 'Charlie', 'Ronnie', 'John', 'Paul', 'George', 'Ringo' ]
+  // var results = [ 'Ronnie', 'Charlie', 'Kieth', 'Mick' ]
+  // var results = [ 'Ringo', 'George', 'Paul', 'John' ]
+  var results = [ 'Ringo', 'Ronnie', 'George', 'Charlie', 'Paul', 'Kieth', 'John', 'Mick' ]
   SearchIndexSearcher({
     indexPath: 'test/sandbox/supergroup'
   }, function (err, si) {
---
+++ test/searchability-test.js
@@ -0,0 +1,434 @@
+const JSONStream = require('JSONStream')
+const Readable = require('stream').Readable
+const SearchIndexAdder = require('../')
+const SearchIndexSearcher = require('search-index-searcher')
+const logLevel = process.env.NODE_ENV || 'info'
+const sandbox = process.env.SANDBOX || 'test/sandbox'
+const test = require('tape')
+
+const getReadStream = function () {
+  const batch = [
+    {
+      id: '1',
+      name: 'Apple Watch',
+      description: 'Receive and respond to notiﬁcations in an instant.',
+      price: '20002',
+      age: '346'
+    },
+    {
+      id: '2',
+      name: 'Victorinox Swiss Army',
+      description: 'You have the power to keep time moving with this Airboss automatic watch.',
+      price: '99',
+      age: '33342'
+    },
+    {
+      id: '3',
+      name: "Versace Men's Swiss",
+      description: "Versace Men's Swiss Chronograph Mystique Sport Two-Tone Ion-Plated Stainless Steel Bracelet Watch",
+      price: '4716',
+      age: '8293'
+    },
+    {
+      id: '4',
+      name: "CHARRIOL Men's Swiss Alexandre",
+      description: 'With CHARRIOLs signature twisted cables, the Alexander C timepiece collection is a must-have piece for lovers of the famed brand.',
+      price: '2132',
+      age: '33342'
+    },
+    {
+      id: '5',
+      name: "Ferragamo Men's Swiss 1898",
+      description: 'The 1898 timepiece collection from Ferragamo offers timeless luxury.',
+      price: '99999',
+      age: '33342'
+    },
+    {
+      id: '6',
+      name: 'Bulova AccuSwiss',
+      description: 'The Percheron Treble timepiece from Bulova AccuSwiss sets the bar high with sculpted cases showcasing sporty appeal. A Manchester United® special edition.',
+      price: '1313',
+      age: '33342'
+    },
+    {
+      id: '7',
+      name: 'TW Steel',
+      description: 'A standout timepiece that boasts a rich heritage and high-speed design. This CEO Tech watch from TW Steel sets the standard for elite. Armani',
+      price: '33333',
+      age: '33342'
+    },
+    {
+      id: '8',
+      name: 'Invicta Bolt Zeus ',
+      description: "Invicta offers an upscale timepiece that's as full of substance as it is style. From the Bolt Zeus collection.",
+      price: '8767',
+      age: '33342'
+    },
+    {
+      id: '9',
+      name: 'Victorinox Night Vision ',
+      description: "Never get left in the dark with Victorinox Swiss Army's Night Vision watch. First at Macy's!",
+      price: '1000',
+      age: '33342'
+    },
+    {
+      id: '10',
+      name: 'Armani Swiss Moon Phase',
+      description: 'Endlessly sophisticated in materials and design, this Emporio Armani Swiss watch features high-end timekeeping with moon phase movement and calendar tracking swiss swiss.',
+      price: '30000',
+      age: '33342'
+    }
+  ]
+  const s = new Readable()
+  batch.forEach(function (item) {
+    s.push(JSON.stringify(item))
+  })
+  s.push(null)
+  return s
+}
+
+test('initialize a search index with no fielded search', function (t) {
+  t.plan(13)
+  SearchIndexAdder({
+    fieldedSearch: false,
+    indexPath: sandbox + '/si-no-fielded-search',
+    logLevel: logLevel
+  }, function (err, indexer) {
+    t.error(err)
+    getReadStream().pipe(JSONStream.parse())
+      .pipe(indexer.defaultPipeline())
+      .pipe(indexer.add())
+      .on('data', function (data) {
+        t.ok(true, ' data recieved')
+      })
+      .on('end', function () {
+        indexer.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('search in a search index with no fielded search', function (t) {
+  var counter = 0
+  t.plan(4)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-no-fielded-search',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search('moon calendar')
+      .on('data', function (data) {
+        t.ok(JSON.parse(data).document.id === '10')
+        t.equals(++counter, 1)
+      }).on('end', function () {
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('search in a search index with no fielded search', function (t) {
+  var results = ['5', '4']
+  var counter = 0
+  t.plan(5)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-no-fielded-search',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search('swiss timepiece')
+      .on('data', function (data) {
+        counter++
+        t.equals(JSON.parse(data).document.id, results.shift())
+      }).on('end', function () {
+        t.equals(counter, 2)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('cant do a fielded search', function (t) {
+  var counter = 0
+  t.plan(3)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-no-fielded-search',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search({
+      query: {
+        AND: {name: ['versace']}
+      }
+    })
+      .on('data', function (data) {
+        counter++
+      }).on('end', function () {
+        t.equals(counter, 0)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('initialize a search index WITH fielded search', function (t) {
+  t.plan(13)
+  SearchIndexAdder({
+    fieldedSearch: true,
+    indexPath: sandbox + '/si-fielded-search-on',
+    logLevel: logLevel
+  }, function (err, indexer) {
+    t.error(err)
+    getReadStream().pipe(JSONStream.parse())
+      .pipe(indexer.defaultPipeline())
+      .pipe(indexer.add())
+      .on('data', function (data) {
+        t.ok(true, ' data recieved')
+      })
+      .on('end', function () {
+        indexer.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('CAN do a fielded search', function (t) {
+  var counter = 0
+  t.plan(4)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-fielded-search-on',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search({
+      query: {
+        AND: {name: ['versace']}
+      }
+    })
+      .on('data', function (data) {
+        t.equals(JSON.parse(data).document.id, '3')
+        counter++
+      }).on('end', function () {
+        t.equals(counter, 1)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('CAN do a fielded search', function (t) {
+  var results = ['9', '3', '10']
+  var counter = 0
+  t.plan(6)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-fielded-search-on',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search({
+      query: {
+        AND: {description: ['swiss', 'watch']}
+      }
+    })
+      .on('data', function (data) {
+        t.equals(JSON.parse(data).document.id, results.shift())
+        counter++
+      }).on('end', function () {
+        t.equals(counter, 3)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('initialize a search index WITH fielded search on specified fields', function (t) {
+  t.plan(13)
+  SearchIndexAdder({
+    fieldedSearch: false,
+    indexPath: sandbox + '/si-fielded-search-specified-fields',
+    logLevel: logLevel
+  }, function (err, indexer) {
+    t.error(err)
+    getReadStream().pipe(JSONStream.parse())
+      .pipe(indexer.defaultPipeline({
+        fieldOptions: {
+          description: {
+            fieldedSearch: true
+          }
+        }
+      }))
+      .pipe(indexer.add())
+      .on('data', function (data) {
+        t.ok(true, ' data recieved')
+      })
+      .on('end', function () {
+        indexer.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('CAN do a fielded search on "description" field', function (t) {
+  var results = ['9', '3', '10']
+  var counter = 0
+  t.plan(6)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-fielded-search-specified-fields',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search({
+      query: {
+        AND: {description: ['swiss', 'watch']}
+      }
+    })
+      .on('data', function (data) {
+        t.equals(JSON.parse(data).document.id, results.shift())
+        counter++
+      }).on('end', function () {
+        t.equals(counter, 3)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('CANT do a fielded search on name field', function (t) {
+  var counter = 0
+  t.plan(3)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-fielded-search-specified-fields',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search({
+      query: {
+        AND: {name: ['versace']}
+      }
+    })
+      .on('data', function (data) {
+        counter++
+      }).on('end', function () {
+        t.equals(counter, 0)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('CAN find stuff from name field in wildcard', function (t) {
+  var counter = 0
+  t.plan(4)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-fielded-search-specified-fields',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search({
+      query: {
+        AND: {'*': ['versace']}
+      }
+    })
+      .on('data', function (data) {
+        t.equals(JSON.parse(data).document.id, '3')
+        counter++
+      }).on('end', function () {
+        t.equals(counter, 1)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('initialize a search index WITH only name field searchable', function (t) {
+  t.plan(13)
+  SearchIndexAdder({
+    searchable: false,
+    indexPath: sandbox + '/si-searchable-specified-fields',
+    logLevel: logLevel
+  }, function (err, indexer) {
+    t.error(err)
+    getReadStream().pipe(JSONStream.parse())
+      .pipe(indexer.defaultPipeline({
+        fieldOptions: {
+          name: {
+            searchable: true
+          }
+        }
+      }))
+      .pipe(indexer.add())
+      .on('data', function (data) {
+        t.ok(true, ' data recieved')
+      })
+      .on('end', function () {
+        indexer.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('CAN find stuff from name field in wildcard', function (t) {
+  var counter = 0
+  t.plan(4)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-searchable-specified-fields',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search({
+      query: {
+        AND: {'*': ['versace']}
+      }
+    })
+      .on('data', function (data) {
+        t.looseEquals(JSON.parse(data).document, {
+          age: '8293',
+          description: 'Versace Men\'s Swiss Chronograph Mystique Sport Two-Tone Ion-Plated Stainless Steel Bracelet Watch',
+          id: '3',
+          name: 'Versace Men\'s Swiss',
+          price: '4716' })
+        counter++
+      }).on('end', function () {
+        t.equals(counter, 1)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
+
+test('CANT find stuff from description field in wildcard', function (t) {
+  var counter = 0
+  t.plan(3)
+  SearchIndexSearcher({
+    indexPath: sandbox + '/si-searchable-specified-fields',
+    logLevel: logLevel
+  }, function (err, searcher) {
+    t.error(err)
+    searcher.search({
+      query: {
+        AND: {'*': ['chronograph', 'mystique', 'sport']}
+      }
+    })
+      .on('data', function (data) {
+        counter++
+      }).on('end', function () {
+        t.equals(counter, 0)
+        searcher.close(function (err) {
+          t.error(err)
+        })
+      })
+  })
+})
