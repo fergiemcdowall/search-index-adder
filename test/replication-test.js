7aa4b7471638f445bdb9c3d2014795792c0947bb
Fergie McDowall <fergusmcdowall@gmail.com>
2017-01-05 14:59:19 +0100

    fix tests

--- test/replication-test.js
+++ test/replication-test.js
@@ -51,7 +51,7 @@ test('simple read from replicator (no ops)', function (t) {
       i++
     })
     .on('end', function () {
-      t.equal(i, 3101)
+      t.equal(i, 3108)
     })
 })
 
@@ -93,7 +93,7 @@ test('simple read from replicated index (no ops)', function (t) {
       i++
     })
     .on('end', function () {
-      t.equal(i, 3101)
+      t.equal(i, 3108)
     })
 })
 
@@ -138,7 +138,7 @@ test('validate gzip replication', function (t) {
       i++
     })
     .on('end', function () {
-      t.equal(i, 3101)
+      t.equal(i, 3108)
     })
 })
 
