f5dc643876e3dac6d1504aec1a9217e18d1e0472
Fergie McDowall <fergusmcdowall@gmail.com>
2017-07-06 15:02:19 +0200

    refactor(prettiness): refactor
    more readable code, avoid circular references

--- index.js
+++ index.js
@@ -6,158 +6,25 @@ standalone
 
 */
 
-const DBEntries = require('./lib/delete.js').DBEntries
-const DBWriteCleanStream = require('./lib/replicate.js').DBWriteCleanStream
-const DBWriteMergeStream = require('./lib/replicate.js').DBWriteMergeStream
-const DocVector = require('./lib/delete.js').DocVector
-const IndexBatch = require('./lib/add.js').IndexBatch
-const JSONStream = require('JSONStream')
-const Readable = require('stream').Readable
-const RecalibrateDB = require('./lib/delete.js').RecalibrateDB
 const bunyan = require('bunyan')
-const del = require('./lib/delete.js')
-const docProc = require('docproc')
 const levelup = require('levelup')
-const pumpify = require('pumpify')
-
-const async = require('async')
+const API = require('./lib/API.js')
 
 module.exports = function (givenOptions, callback) {
   getOptions(givenOptions, function (err, options) {
-    var Indexer = {}
+    const api = API(options)
+    const Indexer = {}
     Indexer.options = options
-
-    var q = async.queue(function (batch, done) {
-      var s
-      if (batch.operation === 'add') {
-        s = new Readable({ objectMode: true })
-        batch.batch.forEach(function (doc) {
-          s.push(doc)
-        })
-        s.push(null)
-        s.pipe(Indexer.defaultPipeline(batch.batchOps))
-          .pipe(Indexer.add())
-          .on('finish', function () {
-            return done()
-          })
-          .on('error', function (err) {
-            return done(err)
-          })
-      } else if (batch.operation === 'delete') {
-        s = new Readable()
-        batch.docIds.forEach(function (docId) {
-          s.push(JSON.stringify(docId))
-        })
-        s.push(null)
-        s.pipe(Indexer.deleteStream(options))
-          .on('data', function () {
-            // nowt
-          })
-          .on('end', function () {
-            done(null)
-          })
-      }
-    }, 1)
-
-    Indexer.add = function () {
-      return pumpify.obj(
-        new IndexBatch(Indexer),
-        new DBWriteMergeStream(options)
-      )
-    }
-
-    Indexer.concurrentAdd = function (batchOps, batch, done) {
-      q.push({
-        batch: batch,
-        batchOps: batchOps,
-        operation: 'add'
-      }, function (err) {
-        done(err)
-      })
-    }
-
-    Indexer.concurrentDel = function (docIds, done) {
-      q.push({
-        docIds: docIds,
-        operation: 'delete'
-      }, function (err) {
-        done(err)
-      })
-    }
-
-    Indexer.close = function (callback) {
-      options.indexes.close(function (err) {
-        while (!options.indexes.isClosed()) {
-          options.log.debug('closing...')
-        }
-        if (options.indexes.isClosed()) {
-          options.log.debug('closed...')
-          callback(err)
-        }
-      })
-    }
-
-    Indexer.dbWriteStream = function (streamOps) {
-      streamOps = Object.assign({}, { merge: true }, streamOps)
-      if (streamOps.merge) {
-        return new DBWriteMergeStream(options)
-      } else {
-        return new DBWriteCleanStream(options)
-      }
-    }
-
-    Indexer.defaultPipeline = function (batchOptions) {
-      batchOptions = Object.assign({}, options, batchOptions)
-      return docProc.pipeline(batchOptions)
-    }
-
-    Indexer.deleteStream = function (options) {
-      return pumpify.obj(
-        new DocVector(options),
-        new DBEntries(options),
-        new RecalibrateDB(options)
-      )
-    }
-
-    Indexer.deleter = function (docIds, done) {
-      const s = new Readable()
-      docIds.forEach(function (docId) {
-        s.push(JSON.stringify(docId))
-      })
-      s.push(null)
-      s.pipe(Indexer.deleteStream(options))
-        .on('data', function () {
-          // nowt
-        })
-        .on('end', function () {
-          done(null)
-        })
-    }
-
-    Indexer.feed = function (ops) {
-      if (ops && ops.objectMode) {
-        // feed from stream of objects
-        return pumpify.obj(
-          Indexer.defaultPipeline(ops),
-          Indexer.add(ops)
-        )
-      } else {
-        // feed from stream of strings
-        return pumpify(
-          JSONStream.parse(),
-          Indexer.defaultPipeline(ops),
-          Indexer.add(ops)
-        )
-      }
-    }
-
-    Indexer.flush = function (APICallback) {
-      del.flush(options, function (err) {
-        return APICallback(err)
-      })
-    }
-
-    //  return Indexer
+    Indexer.add = api.add
+    Indexer.concurrentAdd = api.concurrentAdd
+    Indexer.concurrentDel = api.concurrentDel
+    Indexer.close = api.close
+    Indexer.dbWriteStream = api.dbWriteStream
+    Indexer.defaultPipeline = api.defaultPipeline
+    Indexer.deleteStream = api.deleteStream
+    Indexer.deleter = api.deleter
+    Indexer.feed = api.feed
+    Indexer.flush = api.flush
     return callback(err, Indexer)
   })
 }
